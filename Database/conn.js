55529d45925169822fa90fc47083f097801f7c5d
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-02-24 12:01:35 +0530

    push frontend code

--- Database/conn.js
+++ Database/conn.js
@@ -1,22 +1,27 @@
 const mysql = require('mysql');
 require("dotenv").config();
-
+const path = require('path');
 
 const connection = mysql.createConnection({
-  host: process.env.HOSTNAME,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DB_NAME,
-  port:process.env.PORT
+  host: 'mysql-cfa89fb-prajapatikashyap14-72ed.a.aivencloud.com',
+  user: 'avnadmin',
+  password: process.env.DB_PASSWORD,
+  database: 'BOOK_READING_CRUD',
+  port: 14645,
+  authPlugin: 'caching_sha2_password',
+  ssl: {
+    ca: path.resolve(__dirname, 'Backend', 'Database', 'ca.pem'), // Use path.resolve to get absolute path
+    rejectUnauthorized: false
+  }
 });
+
 
 connection.connect((err) => {
     if (err) {
       console.error('Error connecting to MySQL:', err);
       process.exit(1);
     }
-    console.log('Connected to MySQL 🗄️  SucessFully..');
-  });
-  
+    console.log('Connected to MySQL 🗄️  Successfully..');
+});
 
 module.exports = connection;
